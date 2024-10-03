7d57173b934128b2d465dc093033e1088d37e18f
sandyadzim <sandy.adzim@gmail.com>
2024-10-03 15:08:07 +0700

    feat: auth with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,14 @@ import { Inter as FontSans } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
 import { Metadata } from 'next'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -20,12 +28,31 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body
-        className={cn('min-h-screen font-sans antialiased', fontSans.variable)}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: '#3371FF',
+          fontSize: '16px',
+        },
+      }}
+    >
+      <html lang="en">
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased',
+            fontSans.variable
+          )}
+        >
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
