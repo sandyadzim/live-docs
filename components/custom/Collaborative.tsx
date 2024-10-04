62ddc6397e1df8fccd3e7c637809a087275541f9
sandyadzim <sandy.adzim@gmail.com>
2024-10-04 22:10:21 +0700

    feat: document detail, update title

--- components/custom/Collaborative.tsx
+++ components/custom/Collaborative.tsx
@@ -4,15 +4,116 @@ import { ClientSideSuspense, RoomProvider } from '@liveblocks/react/suspense'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/custom/Header'
 import Loader from '@/components/custom/Loader'
+import { Input } from '@/components/ui/input'
+import Image from 'next/image'
 import ActiveCollaborators from '@/components/custom/ActiveCollaborators'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { useEffect, useRef, useState } from 'react'
+import { updateDocument } from '@/lib/actions/room.action'
 
 const Collaborative = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
+  const currentUserType = 'editor'
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
+  const [isEditing, setIsEditing] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const inputRef = useRef<HTMLDivElement>(null)
+
+  const updateTitleHandler = async (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === 'Enter') {
+      setIsLoading(true)
+
+      try {
+        if (documentTitle !== roomMetadata.title) {
+          const updatedDocument = await updateDocument(roomId, documentTitle)
+
+          if (updatedDocument) {
+            setIsEditing(false)
+          }
+        }
+      } catch (error) {
+        console.error(error)
+      }
+
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    const handleClickOutside = async (e: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setIsLoading(true)
+
+        await updateDocument(roomId, documentTitle)
+
+        setIsEditing(false)
+        setIsLoading(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [roomId, documentTitle])
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isEditing])
+
   return (
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<Loader />}>
         <div className="collaborative-room">
           <Header>
+            <div
+              ref={containerRef}
+              className="flex w-fit items-center justify-center gap-2"
+            >
+              {isEditing && !isLoading ? (
+                <Input
+                  type="text"
+                  value={documentTitle}
+                  ref={inputRef}
+                  placeholder="Enter title"
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disable={!isEditing}
+                  className="document-title-input"
+                />
+              ) : (
+                <>
+                  <p className="document-title">{documentTitle}</p>
+                </>
+              )}
+
+              {currentUserType === 'editor' && !isEditing && (
+                <Image
+                  src="/assets/icons/edit.svg"
+                  alt="edit"
+                  width={24}
+                  height={24}
+                  onClick={() => setIsEditing(true)}
+                  className="pointer"
+                />
+              )}
+
+              {currentUserType !== 'editor' && !isEditing && (
+                <p className="view-only-tag">View only</p>
+              )}
+
+              {isLoading && <p className="text-sm text-gray-400">saving...</p>}
+            </div>
+
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveCollaborators />
 
@@ -24,7 +125,6 @@ const Collaborative = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
               </SignedIn>
             </div>
           </Header>
-
           <Editor />
         </div>
       </ClientSideSuspense>
--- lib/actions/room.action.ts
+++ lib/actions/room.action.ts
@@ -58,6 +58,22 @@ export const getDocument = async ({
   }
 }
 
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    })
+
+    revalidatePath(`/documents/${roomId}`)
+
+    return parseStringify(updatedRoom)
+  } catch (error) {
+    console.log(`Error happened while updating a room: ${error}`)
+  }
+}
+
 export const getDocuments = async (email: string) => {
   try {
     const rooms = await liveblocks.getRooms({ userId: email })
