d489a4d41f297ea672e18b1927964742055c0ac9
sandyadzim <sandy.adzim@gmail.com>
2024-10-08 16:36:23 +0700

    feat: header home

--- app/(root)/editor/page.tsx
+++ app/(root)/editor/page.tsx
@@ -21,7 +21,7 @@ export default async function EditorPage() {
 
   return (
     <main className="home-container relative">
-      <Header className="sticky left-0 top-0">
+      <Header isHome className="sticky left-0 top-0">
         <div className="flex items-center gap-2 lg:gap-4">
           <Notifications />
           <SignedIn>
