0554156fcc36a1dc50d092f61e85762f2d46eb10
sandyadzim <sandy.adzim@gmail.com>
2024-10-11 16:18:06 +0700

    config: update favicon

--- app/(root)/editor/page.tsx
+++ app/(root)/editor/page.tsx
@@ -30,7 +30,7 @@ export default async function EditorPage() {
         </div>
       </Header>
 
-      {roomDocuments.data.length > 0 ? (
+      {roomDocuments?.data.length > 0 ? (
         <div className="document-list-container z-20">
           <div className="document-list-title">
             <h3 className="text-28-semibold">All documents</h3>
